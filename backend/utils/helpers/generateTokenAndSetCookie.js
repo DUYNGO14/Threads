466a38e71e226bb43ee24f67e4a6bceb3a837761
Ngô Duy <ngoduy07142003@gmail.com>
2025-03-28 12:43:08 +0700

    fix bug 🙃

--- backend/utils/helpers/generateTokenAndSetCookie.js
+++ backend/utils/helpers/generateTokenAndSetCookie.js
@@ -1,5 +1,8 @@
 import jwt from "jsonwebtoken";
-import { JWT_EXPIRES_IN, JWT_EXPIRES_IN_REFRESH } from "../../constants.js";
+import {
+  JWT_EXPIRES_IN,
+  JWT_EXPIRES_IN_REFRESH,
+} from "../../constants/token.js";
 const generateTokenAndSetCookie = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: JWT_EXPIRES_IN,
